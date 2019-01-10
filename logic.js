857a050e53b941451b7cb1e15dec7b3ac3281fa2
Lalita Eranki <lalitaeranki@gmail.com>
2019-01-09 21:20:20 -0800

    changes added

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
- const API_KEY=con.API_KEY;
+
  const queryurl="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
  const tectonicPlatesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
 
